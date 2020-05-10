d132c2d93dddc9a5e437fdf69b82cace1377c079
Andres Gomez <andres.f.gomez-nd@disney.com>
2020-05-10 18:02:11 -0500

    Add new public function

--- calculator.go
+++ calculator.go
@@ -1,8 +1,16 @@
 package mygocalculator
 
+import (
+	"fmt"
+)
+
 type Calculator struct {
 }
 
 func (oper *Calculator) Add(a int, b int) (c int) {
 	return a + b
+}
+
+func Start() {
+	fmt.Println("Starting calculator")
 }
